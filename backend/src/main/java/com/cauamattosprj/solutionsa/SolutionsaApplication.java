72d88ac9d1c2e37c6e1edf0fea2b0f11a615fe3d
Caua Mattos <caua.h4k@gmail.com>
2026-03-12 17:53:09 -0300

    first commit

---
+++ backend/src/main/java/com/cauamattosprj/solutionsa/SolutionsaApplication.java
@@ -0,0 +1,13 @@
+package com.cauamattosprj.solutionsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionsaApplication.class, args);
+	}
+
+}
