06667cee06dcdc232c91b7e6284d159ee38b3119
Caua Mattos <caua.h4k@gmail.com>
2026-03-13 20:46:11 -0300

    feat: enable JPA auditing

--- backend/src/main/java/com/cauamattosprj/solutionsa/SolutionsaApplication.java
+++ backend/src/main/java/com/cauamattosprj/solutionsa/SolutionsaApplication.java
@@ -2,8 +2,10 @@ package com.cauamattosprj.solutionsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SolutionsaApplication {
 
 	public static void main(String[] args) {
